51a24c4dfb40eb4e48186a68edca2ecc8c9f073e
akirtovskis <arturs.kirtovskis@gmail.com>
2018-04-14 14:56:50 +0300

    Add PrivateRoute funcionality

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
 import {createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
 import expensesReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -11,6 +13,7 @@ export default () => {
     combineReducers({
       expenses : expensesReducer,
       filters : filterReducer,
+      auth : authReducer,
     }),
     composeEnhancers(applyMiddleware(thunk)),
   )
---
+++ src/tests/reducers/auth.test.js
@@ -0,0 +1,23 @@
+import authReducer from '../../reducers/auth';
+
+test('should set default state', () => {
+  const state = authReducer(undefined, {type:'@@INIT'});
+  expect(state).toEqual({});
+});
+
+test('should set uid for login', () => {
+  const action = {
+    type:'LOGIN',
+    uid:'123'
+  };
+  const state = authReducer({}, action);
+  expect(state.uid).toBe(action.uid);
+});
+
+test('should clear uid for logout', () => {
+  const action = {
+    type:'LOGOUT'
+  };
+  const state = authReducer({uid:'anything'}, action);
+  expect(state).toEqual({});
+});
